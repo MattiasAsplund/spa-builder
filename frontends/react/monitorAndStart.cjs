3add3b070e3e82cdeb14b591e83847c274c4df37
Mattias Asplund Gonzalez <mattias@mattiasasplund.se>
2024-03-25 17:12:35 +0100

    npm install & npm run dev

--- frontends/react/monitorAndStart.cjs
+++ frontends/react/monitorAndStart.cjs
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process');
+const { spawn, exec } = require('child_process');
 
 // Start command1
 const command1 = spawn('node', ['../bff.cjs'], { shell: true, stdio: ['inherit', 'pipe', 'pipe'] });
@@ -7,10 +7,8 @@ const onDataHandler = (data) => {
   const dataStr = data.toString();
   console.log(dataStr); // Log the message (remove the xcommand1 prefix if not needed)
   if (dataStr.includes('Server starting on port 8099...')) {
-    // Start command2
-    console.log("Starting command");
-    spawn('npm', ['install'], { shell: true, stdio: 'inherit' });
-    spawn('npm', ['run', 'dev'], { shell: true, stdio: 'inherit' });
+    exec('npm install');
+    exec('npm run dev');
   }
 };
 
