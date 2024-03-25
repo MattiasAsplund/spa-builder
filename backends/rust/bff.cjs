5409bb9033fc2ca252064abb7be6e7ae1ab05faa
Mattias Asplund Gonzalez <mattias@mattiasasplund.se>
2024-03-25 21:43:42 +0100

    Remove monitorAndStart.cjs

--- backends/rust/bff.cjs
+++ backends/rust/bff.cjs
@@ -1,5 +1,5 @@
 const { exec, spawn } = require('child_process');
 
 console.log("Installing backend dependencies...");
-exec("cargo run", { cwd: '..' }, (error, stdout, stderr) => {        
+exec("cargo", ["run"], { cwd: '..' }, (error, stdout, stderr) => {        
 });
