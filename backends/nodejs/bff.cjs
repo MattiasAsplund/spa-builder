8e2d86fd9f89c1b3447c08a20b8173ed3f47a520
Mattias Asplund Gonzalez <mattias@mattiasasplund.se>
2024-03-25 20:13:46 +0100

    npm start

--- backends/nodejs/bff.cjs
+++ backends/nodejs/bff.cjs
@@ -1,24 +1,8 @@
-const { exec, spawn } = require('child_process');
+const { spawn } = require('child_process');
 const util = require('util');
-const execAsync = util.promisify(exec);
 
 const runBff = (async () => {
-    await execAsync("npm install", { cwd: '..' });
-    console.log("npm install");
-
-    let childProcess = spawn("node", ["server.js"], { cwd: '..' });
-    
-    childProcess.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-    
-    childProcess.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-    
-    childProcess.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });    
+    const serverProcess = spawn("node", ["server.js"], { stdio: 'inherit', cwd: '..' });
 })
 
 runBff();
